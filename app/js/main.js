4a7ad86486fa54199cac82ab7dd3f3cb4e531a4d
andreyarnapolin <andreyarnapolin@yahoo.com>
2020-02-03 03:29:31 +0200

    gallery

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
 
-(function($){
-	$(document).ready(function() {
-		// Code
-
-	});
+(function($) {
+  $(document).ready(function() {
+    // Code
+    //Fancybox
+    $(".photo").fancybox({});
+    //remove or add attribute when screen is different width
+    $(document).ready(function() {
+      if (window.innerWidth > 1024) {
+        $(".gallery__show").removeAttr("data-fancybox");
+      }
+    });
+    $(window).on("resize", function() {
+      if ($(window).width() >= 1024) {
+        $(".gallery__show").removeAttr("data-fancybox");
+      } else {
+        $(".gallery__show").attr({
+          "data-fancybox": "gallery"
+        });
+      }
+    });
+  });
 })(jQuery);
