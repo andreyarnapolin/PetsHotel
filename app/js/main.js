1e25dae5816cbee6d697b8c5b1b02c4e505f1deb
oleksandrzhovtiak <zhovtyak25@gmail.com>
2020-02-06 22:12:07 +0200

    m

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,17 @@
 "use strict";
 
-(function($) {
-  $(document).ready(function() {
+(function ($) {
+  $(document).ready(function () {
     // Code
     //Fancybox
     $(".photo").fancybox({});
     //remove or add attribute when screen is different width
-    $(document).ready(function() {
+    $(document).ready(function () {
       if (window.innerWidth > 1024) {
         $(".gallery__show").removeAttr("data-fancybox");
       }
     });
-    $(window).on("resize", function() {
+    $(window).on("resize", function () {
       if ($(window).width() >= 1024) {
         $(".gallery__show").removeAttr("data-fancybox");
       } else {
@@ -22,3 +22,21 @@
     });
   });
 })(jQuery);
+
+{/* <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script> */ }
+
+// <script type="text/javascript">
+
+$(document).ready(function () {
+  $('.tab a').on('click', function (e) {
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    var href = $(this).attr('href');
+    $('.forms > form').hide();
+    $(href).fadeIn(500);
+  });
+
+});
