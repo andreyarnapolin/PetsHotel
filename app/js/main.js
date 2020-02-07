2a5cb75ded036ffdec8c99663fd73316bc2ff3bf
andreyarnapolin <andreyarnapolin@yahoo.com>
2020-02-07 03:24:19 +0200

    review added window

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,33 @@
 "use strict";
 
-(function ($) {
-  $(document).ready(function () {
-    // Code
+(function($) {
+  $(document).ready(function() {
+    // FORM LOGIN REGISTRATION
+    // $(".tab a").on("click", function(e) {
+    //   e.preventDefault();
+
+    //   $(this)
+    //     .parent()
+    //     .addClass("active");
+    //   $(this)
+    //     .parent()
+    //     .siblings()
+    //     .removeClass("active");
+
+    //   let href = $(this).attr("href");
+    //   $(".forms > form").hide();
+    //   $(href).fadeIn(500);
+    // });
+
     //Fancybox
     $(".photo").fancybox({});
     //remove or add attribute when screen is different width
-    $(document).ready(function () {
+    $(document).ready(function() {
       if (window.innerWidth > 1024) {
         $(".gallery__show").removeAttr("data-fancybox");
       }
     });
-    $(window).on("resize", function () {
+    $(window).on("resize", function() {
       if ($(window).width() >= 1024) {
         $(".gallery__show").removeAttr("data-fancybox");
       } else {
@@ -20,23 +36,51 @@
         });
       }
     });
+    //REVIEW WINDOW
+
+    $(".review__link").click(function reviewWindow() {
+      const clonedReviewWrap = $(this.closest(".review__wrap")).clone();
+      const clonedReviewImage = $(this.closest(".review__wrap"))
+        .find(".review__image")
+        .clone();
+      const clonedReviewName = $(this.closest(".review__wrap"))
+        .find(".review__name")
+        .clone();
+      $(clonedReviewWrap)
+        .find(".review__link")
+        .remove();
+      $(clonedReviewWrap)
+        .find(".review__image")
+        .remove();
+      $(clonedReviewWrap)
+        .find(".review__name")
+        .remove();
+      let dialog = document.createElement("div"),
+        dialogOverlay = document.createElement("div"),
+        dialogLink = document.createElement("a");
+
+      dialogOverlay.className = "dialog-overlay";
+      dialog.className = "dialog";
+      dialogLink.className = "dialog__link";
+      $(dialogLink).attr("href", "#");
+      $(dialogLink).text("Назад");
+
+      $(dialog).append(clonedReviewWrap);
+      $(clonedReviewWrap)
+        .find(".review__date")
+        .after(clonedReviewImage);
+      $(clonedReviewWrap)
+        .find(".review__date")
+        .after(clonedReviewName);
+      $(clonedReviewWrap)
+        .find(".review__info")
+        .after(dialogLink);
+      document.body.append(dialogOverlay, dialog);
+
+      $(".dialog__link").click(function reviewClose() {
+        dialogOverlay.remove();
+        dialog.remove();
+      });
+    });
   });
 })(jQuery);
-
-{/* <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script> */ }
-
-// <script type="text/javascript">
-
-$(document).ready(function () {
-  $('.tab a').on('click', function (e) {
-    e.preventDefault();
-
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-
-    var href = $(this).attr('href');
-    $('.forms > form').hide();
-    $(href).fadeIn(500);
-  });
-
-});
